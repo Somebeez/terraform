89be07ebf8421cf1772a6ce08110f299786272bc
mearegTG <mearegtsega@gmail.com>
2022-10-18 14:03:56 -0400

    added terraform backend

--- main.tf
+++ main.tf
@@ -1,7 +1,22 @@
 provider "aws" {
-  profile = "default"
+ // profile = "default"
+ access_key = "AKIA6FY5YJAFYYKIXPXK"
+ secret_key = "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"
   region  = var.region
 }
+
+terraform {
+  backend "remote" {
+    organization = "meareg-sol"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+}
+
+
+
 
 
 
